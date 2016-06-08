5de92cdcaafb37a1040c9a1eaeb0edae96e2dc59
YaoShangTseng <yakushou730@gmail.com>
2016-06-08 19:42:32 +0800

    Add 2 columns in User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160608072132) do
+ActiveRecord::Schema.define(version: 20160608073442) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
@@ -46,6 +46,8 @@ ActiveRecord::Schema.define(version: 20160608072132) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
