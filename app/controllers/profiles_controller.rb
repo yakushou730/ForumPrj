4b900af42e7c0cdd858ce723131b43dde3af68e3
YaoShangTseng <yakushou730@gmail.com>
2016-06-18 12:41:28 +0800

    Apply showing favorite topics in user's profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,6 +4,25 @@ class ProfilesController < ApplicationController
     @profile = User.find(params[:id])
     @topics = Topic.where("user_id = #{params[:id]}")
     @comments = Comment.where("user_id = #{params[:id]}")
+
+    favorite_topic_ids = []
+    favorite_topics = UserTopicFavorite.where(:user_id=> params[:id])
+
+    @favorites = []
+    favorite_topics.each do |favorite_topic|
+      @favorites << Topic.find(favorite_topic.topic_id)
+    end
+
+    # favorite_topics.each do |t|
+    #   favorite_topic_ids << t.topic_id
+    # end
+
+    # @user_favorite_topics = []
+
+    # favorite_topic_ids.each do |id|
+    #   @user_favorite_topics << Topic.find(id)
+    # end
+
   end
 
 end
